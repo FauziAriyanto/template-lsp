266962ad85bff3d4bf21bf80a8a2a3a0a9c9bee6
FauziAriyanto <fauziariyanto2018@gmail.com>
2025-04-26 16:16:41 +0700

    update icon

--- resources/views/keranjang/index.blade.php
+++ resources/views/keranjang/index.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round"d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" /></svg>
             {{ __('Keranjang Belanja') }}
         </h2>
     </x-slot>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -98,8 +100,8 @@
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300"
                                     colspan="9">
                                     <a href="{{ route('keranjang.create') }}"
-                                        class="text-blue-600 hover:text-blue-900 underline">
-                                        + Tambahkan produk ke keranjang belanja
+                                        class=" font-bold text-blue-600 hover:text-blue-900 ">
+                                        Tambahkan produk ke keranjang belanja
                                     </a>
                                 </td>
                             </tr>
@@ -117,8 +119,7 @@
                         @csrf
                         <label for="pay_total" class="block text-gray-700 dark:text-gray-300 font-semibold mb-1">Jumlah
                             Bayar</label>
-                        <input type="text" name="pay_total" id="pay_total" placeholder=""
-                            value="{{ old('pay_total') }}"
+                        <input type="text" name="pay_total" id="pay_total" placeholder="" value="{{ old('pay_total') }}"
                             class="w-full rounded gray-300 mb-2 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 @error('pay_total') border-red-500 @enderror"
                             oninput="this.value = this.value.replace(/[^0-9]/g, '')" />
                         @error('pay_total')
